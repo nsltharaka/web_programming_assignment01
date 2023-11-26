6b013caf4c4a516993ada3ea8fff8d79d6eb79a1
nsltharaka <nsltharaka@gmail.com>
2023-11-27 00:26:55 +0530

    added header

--- app/Views/layout/mainLayout.php
+++ app/Views/layout/mainLayout.php
@@ -5,25 +5,50 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/main.css">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
 
     <title>Home</title>
 </head>
 
 <body>
 
-    <div class="bg-white w-100 position-fixed top-0" style="height: 70px; z-index: 1000;">
-        <h1>HOME | hello, <?= session('user')['first_name'] ?? "user" ?></h1>
-    </div>
+    <nav class="navbar">
+        <div class="navbar--main-logo">
+            <img src="images/logo.png" alt="">
+        </div>
+        <ul class="navbar--list">
+            <li><a href="">Home</a></li>
+            <li><a href="">Vehicles</a></li>
+            <li><a href="">about</a></li>
+        </ul>
 
-    <main class="position-relative" style="margin-top: 80px;">
+        <?php if (session('user')) { ?>
+
+            <div class="navbar--form greet">
+                <span>Hello, <?= session('user')['first_name'] ?></span>
+                <img src="images/user_icon.png" alt="">
+            </div>
+
+        <?php } else { ?>
+
+            <div class="navbar--form">
+                <ul class="navbar--list">
+                    <li><a href="/user">login</a></li>
+                </ul>
+            </div>
+
+        <?php } ?>
+    </nav>
+
+    <main>
         <?= $this->renderSection('content'); ?>
     </main>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
 </body>
 
 </html>
